5517f057b27742541803d78b6732036f53de12f9
AcerNg548 <105017838+AcerNg548@users.noreply.github.com>
2023-01-06 19:19:49 +0100

    Update requests.js

--- utils/requests.js
+++ utils/requests.js
@@ -5,8 +5,8 @@ export default{
         title: "Trending",
         url: `/trending/all/week?api_key=${API_KEY}&language=en-US`
     },
-    fetchTopRated:{
+    fetchTopRated: {
         title: "Top Rated",
-        url: `/movie/top_rated?api_key=${API_KEY}&language=en-US`
+        url: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
     },
 }
